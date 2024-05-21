0b95a027b553595f53b8721e383b41a28799915b
Barry <Barry.b.fourie@gmail.com>
2024-05-21 11:18:13 +0000

    Enable cors for testing

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin:'https://barry-8081.entrostat.dev',
+    methods:'POST',
+    credentials: true
+  })
   await app.listen(8080);
 }
 bootstrap();
